d1a597c6ec348c68d1c4b5fc19fddd52f34f6b08
Aidan Dow <aidan@Aidans-MacBook-Pro.local>
2021-01-15 14:43:32 +0000

    Chapter 5 💪

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from rango.models import Catergory, Page
 
 # Register your models here.
+admin.site.register(Catergory)
+admin.site.register(Page)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Catergory(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+
+	class Meta:
+		verbose_name_plural = "Catergories"
+
+	def __str__(self):
+		return self.name
+
+
+class Page(models.Model):
+	catergory = models.ForeignKey(Catergory, on_delete=models.CASCADE)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.title
