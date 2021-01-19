fa4db8c4a956e90fcb3726d233beabb291ae87f4
Aidan Dow <aidan@Aidans-MacBook-Pro.local>
2021-01-19 15:56:28 +0000

    Chapter 5 Fails 1 test :(

--- populate_rango.py
+++ populate_rango.py
@@ -3,7 +3,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 import django
 django.setup() 
-from rango.models import Catergory, Page
+from rango.models import Category, Page
 
 def populate():
 	python_pages = [
@@ -28,20 +28,22 @@ def populate():
 		{'title':'Flask',
 		 'url':'http://flask.pocoo.org'} ]
 
-	cats = {'Python': {'pages': python_pages},
-			'Django': {'pages': django_pages},
-			'Other Frameworks': {'pages': other_pages} }
+	cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+			'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+			'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
 
 
 	def add_page(cat, title, url, views=0):
-		p = Page.objects.get_or_create(catergory=cat, title=title)[0]
+		p = Page.objects.get_or_create(category=cat, title=title)[0]
 		p.url = url
 		p.views = views
 		p.save()
 		return p
 
-	def add_cat(name):
-		c = Catergory.objects.get_or_create(name=name)[0]
+	def add_cat(name, views=0, likes=0):
+		c = Category.objects.get_or_create(name=name)[0]
+		c.views = views
+		c.likes = likes
 		c.save()
 		return c
 
@@ -49,12 +51,12 @@ def populate():
 	# The code below goes through the cats dictionary, then adds each category,
 	 # and then adds all the associated pages for that category.
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(name=cat, views=cat_data['views'], likes=cat_data['likes'])
 		for p in cat_data['pages']:
 			add_page(c, p['title'], p['url'])
 
-	for c in Catergory.objects.all():
-		for p in Page.objects.filter(catergory=c):
+	for c in Category.objects.all():
+		for p in Page.objects.filter(category=c):
 			print(f'- {c}: {p}')
 
 
--- rango/models.py
+++ rango/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Catergory(models.Model):
+class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 
 	class Meta:
-		verbose_name_plural = "Catergories"
+		verbose_name_plural = "Categories"
 
 	def __str__(self):
 		return self.name
 
 
 class Page(models.Model):
-	catergory = models.ForeignKey(Catergory, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	title = models.CharField(max_length=128)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
