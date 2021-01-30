6732f0f0b7743a9be5be1f262c721780e4b4c60f
Aidan Dow <aidan@Aidans-MacBook-Pro.local>
2021-01-30 21:21:00 +0000

    Chapter 10 ✅🎉

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -21,11 +22,17 @@ def index(request):
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
 
+	visitor_cookie_handler(request)
+	
+
+	response = render(request, 'rango/index.html', context=context_dict)
 	# Render the response and then send it back!
-	return render(request, 'rango/index.html', context=context_dict)
+	return response
 
 def about(request):
-	context_dict = {'boldmessage': 'This tutorial has been put together by Aidan!'}
+	context_dict = {}
+	context_dict['boldmessage'] = 'This tutorial has been put together by Aidan!'
+	context_dict['visits'] = request.session['visits']
 	return render(request, 'rango/about.html', context=context_dict)
 
 
@@ -194,6 +201,29 @@ def restricted(request):
 def user_logout(request):
 	logout(request)
 	return redirect(reverse('rango:index'))
+
+
+# Manage Cookies
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits','1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+	if (datetime.now() - last_visit_time).days > 0:
+		visits += 1
+		request.session['last_visit'] = str(datetime.now)
+	else:
+		request.session['last_visit'] = last_visit_cookie
+
+	request.session['visits'] = visits
+
 
 
 
